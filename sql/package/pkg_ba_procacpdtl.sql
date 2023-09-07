5bf56b2ba2b8be5c81beb9b07c1c4743003cafbf
WilliamChen <william-chen@nec.com.tw>
2023-09-07 14:14:48 +0800

    [BABAWEB-88][William][產製媒體後，應收未收及已收程式修正]

--- sql/package/pkg_ba_procacpdtl.sql
+++ sql/package/pkg_ba_procacpdtl.sql
@@ -473,15 +473,14 @@ is
                 --判斷該帳務資料是否已有應收款立帳資料
                 if nvl(trim(v_dataCur.TYPEMK),' ')<>'0' or (nvl(trim(v_dataCur.TYPEMK),' ')='0' and nvl(trim(v_dataCur.RECSEQ),' ')<>' ') then
                     if nvl(trim(v_dataCur.TYPEMK),' ')<>'0' and nvl(trim(v_dataCur.RECSEQ),' ')=' ' then
-                        select BAUNACPDTLID into v_baunacpdtlid
+                        select min(BAUNACPDTLID) into v_baunacpdtlid
                           from BAUNACPDTL t
                          where t.APNO = v_dataCur.MAPNO
                            and t.SEQNO = v_dataCur.MSEQNO
                            and t.ISSUYM = v_dataCur.MISSUYM
                            and t.PAYYM = v_dataCur.MPAYYM
                            and t.PAYKIND = v_dataCur.DISPAYKIND
-                           and rownum=1
-                           order by t.BAUNACPDTLID;
+                           and rownum=1 ;
                     else
                         v_baunacpdtlid := v_dataCur.RECSEQ;
                     end if;
@@ -630,15 +629,14 @@ is
 
                 --取得應收款立帳紀錄編號資料
                    if nvl(trim(v_dataSurCur.TYPEMK),' ')<>'0' and nvl(trim(v_dataSurCur.RECSEQ),' ')=' ' then
-                       select BAUNACPDTLID into v_baunacpdtlid
+                       select min(BAUNACPDTLID) into v_baunacpdtlid
                          from BAUNACPDTL t
                         where t.APNO = v_dataSurCur.MAPNO
                           and t.SEQNO = v_dataSurCur.MSEQNO
                           and t.ISSUYM = v_dataSurCur.MISSUYM
                           and t.PAYYM = v_dataSurCur.MPAYYM
                           and t.PAYKIND = v_dataSurCur.DISPAYKIND
-                          and rownum=1
-                          order by t.BAUNACPDTLID;
+                          and rownum=1;
                    else
                        v_baunacpdtlid := v_dataSurCur.RECSEQ;
                    end if;
